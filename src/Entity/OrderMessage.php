8e5d634732b11432c23cf3cf011aa2d010fbcf3e
Thibault Michel <thibault.michel@viacesi.fr>
2019-11-25 11:23:04 +0100

    review last changes

--- src/Entity/OrderMessage.php
+++ src/Entity/OrderMessage.php
@@ -21,7 +21,7 @@ class OrderMessage implements ResourceInterface
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     protected $id;
 
